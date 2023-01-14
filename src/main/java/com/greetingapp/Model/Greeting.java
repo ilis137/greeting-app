dfac269a3698828e407035668ec6c3458ce7670f
ilis137 <iliyasshahapure1@gmail.com>
2023-01-14 17:22:05 +0530

    add:Using GreetingController return JSON for different HTTP Methods.

---
+++ src/main/java/com/greetingapp/Model/Greeting.java
@@ -0,0 +1,30 @@
+package com.greetingapp.Model;
+
+public class Greeting {
+    private Long id;
+    private String text;
+
+    public Greeting(Long id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+
+    public Greeting() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
